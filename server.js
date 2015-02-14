3add8a7356e5721318040bcba8a65e51b466741b
Jason Als <jasonx2k3@gmail.com>
2015-02-13 21:22:14 -0400

    demo

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ let server = new Hapi.Server();
 
 server.connection({
   host: '0.0.0.0',
-  port: process.env.PORT || 3000
+  port: process.env.PORT || 3000,
+  cors: true 
 });
 
 let people = require('./data/people');
@@ -11,11 +12,17 @@ let people = require('./data/people');
 
 server.route([
   { method: 'get', path: '/', handler: (request, reply) => reply('home') },
-  { method: 'get', path: '/users', handler: (request, reply) => reply(people.all()) },
+  { method: 'post', path: '/users', handler: (request, reply) => reply(people.all()) },
   { method: 'get', path: '/users/xml', handler: (request, reply) => reply(people.xml()) },
   { method: 'get', path: '/user/{id}', handler: (request, reply) => reply( people.findById(request.params.id) ) },
 ]);
 
+
+Array.prototype.push.apply(myArray, myNexxtArray);
+
+var
+
+// b = 2
 
 
 
