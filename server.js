d808ace28e496a37a5a8486b8c35489255587bbf
Jason Als <jasonx2k3@gmail.com>
2015-02-14 08:56:07 -0400

    CORS JSBin attempt #3

--- server.js
+++ server.js
@@ -1,29 +1,39 @@
 let Hapi = require('hapi');
 var simplexml = require('simplexml');
 
-let server = new Hapi.Server();
+let server = new Hapi.Server({
+  connections: {
+    routes: {
+      cors: {
+        headers: ['Authorization', 'Content-Type', 'If-None-Match','X-Requested-With']
+      }
+    }
+  }
+});
 
 server.connection({
   host: '0.0.0.0',
-  port: process.env.PORT || 3000,
-  routes: { cors: true }
+  port: process.env.PORT || 3000
 });
 
 let people = require('./data/people');
 
 let users = (request, reply) => {
-  // console.log(request.headers);
+  console.log(request.headers);
   reply(people.all());
 };
 
 let home = (request, reply) => {
+  server.inject({ url: '/users', headers: { origin: 'http://jsbin.com' } }, function (res) {
+    console.log(res.headers);
+  });
 
   reply('home');
 };
 
 server.route([
   { method: 'get', path: '/', handler: home },
-  { method: 'get', path: '/users', handler: users, config: { cors: true } },
+  { method: 'get', path: '/users', handler: users },
   { method: 'get', path: '/users/xml', handler: (request, reply) => reply(simplexml.toXML( people.all() )) },
   { method: 'get', path: '/user/{id}', handler: (request, reply) => reply( people.findById(request.params.id) ) },
 ]);
